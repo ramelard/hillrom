7691fc98ef4d60cef724e6e43741e978330b422a
Amir-MBP <amirhkarimi@gmail.com>
2018-09-18 10:50:48 -0400

    Modified testFPS.py

--- src/testFPS.py
+++ src/testFPS.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import cv2
+import sys
 import time
+
+if sys.version_info[0] < 3:
+  range_function = xrange
+else:
+  range_function = range
 
 if __name__ == '__main__' :
 
@@ -14,38 +20,36 @@ if __name__ == '__main__' :
     # With webcam get(CV_CAP_PROP_FPS) does not work.
     # Let's see for ourselves.
 
-    if int(major_ver)  < 3 :
+    if int(major_ver) < 3 :
         fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
-        print "Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps)
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
     else :
         fps = video.get(cv2.CAP_PROP_FPS)
-        print "Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps)
+        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
 
 
     # Number of frames to capture
     num_frames = 120;
 
-
-    print "Capturing {0} frames".format(num_frames)
+    print("Capturing {0} frames".format(num_frames))
 
     # Start time
     start = time.time()
 
     # Grab a few frames
-    for i in xrange(0, num_frames) :
+    for i in range_function(0, num_frames) :
         ret, frame = video.read()
-
 
     # End time
     end = time.time()
 
     # Time elapsed
     seconds = end - start
-    print "Time taken : {0} seconds".format(seconds)
+    print("Time taken : {0} seconds".format(seconds))
 
     # Calculate frames per second
     fps  = num_frames / seconds;
-    print "Estimated frames per second : {0}".format(fps);
+    print("Estimated frames per second : {0}".format(fps))
 
     # Release video
     video.release()
